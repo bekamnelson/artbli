536ad234dd67989e093f160e441e067c9a8953ab
bekamnelson <nelsonbekam40@gmail.com>
2026-07-01 14:57:03 +0100

    feat:page login et signup

--- frontend/src/pages/formulaire.jsx
+++ frontend/src/pages/formulaire.jsx
@@ -1,15 +1,37 @@
-function Formulaire(){
+import './../css/form.css';
+import image from './../assets/image.png';
+import { Link } from 'react-router-dom';
+
+function Formulaire() {
     return (
         <form action="#">
-            <label htmlFor="username">nom</label>
-            <input type="text" name="username" id="username" />
-            <label htmlFor="email">email</label>
-            <input type="email" name="email" id="email" />
-            <label htmlFor="password">password</label>
-            <input type="password" name="password" id="password" />
-            <label htmlFor="confirmpassword">password</label>
-            <input type="password" name="confirmpassword" id="confirmpassword" />
-            <input type="submit" value="envoyer" />
+                <div className="informationimage">
+                    <img src={image} alt="ARTBLI" />
+
+            </div>
+            <div className="information">
+                <label htmlFor="username">nom:</label><br />
+                <input type="text" name="username" id="username" />
+            </div>
+            <div className="information">
+                <label htmlFor="emial">email:</label><br />
+                <input type="email" name="email" id="email" />
+            </div>
+            <div className="information">
+                <label htmlFor="password">mot de passe:</label><br />
+                <input type="password" name="password" id="password" />
+            </div>
+            <div className="information">
+                <label htmlFor="confirmpassword">confirmer votre mot de passe:</label><br />
+                <input type="password" name="confirmpassword" id="confirmpassword"  />
+            </div>
+
+            <div className='divbtnsubmit'>
+                <button className='btnsubmit'>envoyer</button>
+            </div>
+            <div>
+                deja un compte?<Link to='/login' >se connecter</Link>
+            </div>
         </form>
     );
 }
--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,12 +1,32 @@
-function Login(){
-    return(
-        <form>
-            <label htmlFor="email">email</label>
-            <input type="email" name="email" id="email" />
-            <label htmlFor="password">password</label>
-            <input type="password" name="password" id="password" />
-            <input type="submit" value="envoyer" />
+import './../css/form.css';
+import image from './../assets/image.png';
+import { Link } from 'react-router-dom';
+
+function Login() {
+    return (
+        <form action="#">
+                <div className="informationimage">
+                    <img src={image} alt="ARTBLI" />
+
+            </div>
+
+            <div className="information">
+                <label htmlFor="emial">email:</label><br />
+                <input type="email" name="email" id="email" />
+            </div>
+            <div className="information">
+                <label htmlFor="password">mot de passe:</label><br />
+                <input type="password" name="password" id="password" />
+            </div>
+           
+            <div className='divbtnsubmit'>
+                <button className='btnsubmit'>envoyer</button>
+            </div>
+            <div>
+                deja un compte?<Link to='/form'>creer un compte </Link>
+            </div>
         </form>
-    )
+    );
 }
+
 export default Login;
